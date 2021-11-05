7a14b8cb738965109753f484b3a28f80552f9f36
Emil Noren <stratosfloor@hotmail.com>
2021-11-05 10:13:18 +0100

    Cleaned up JS-file, mobile-menu and animations is working

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
-console.log('JS-file LOADED');
 
+// From HTML
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.navmenu');
 const closeBtn = document.querySelector('.closeBtn');
 const header = document.querySelector('.header');
 const hero = document.querySelector('.hero');
-const testimonial = document.querySelector('.testimonials');
-const popularChallenges = document.querySelector('.popular-challanges');
+const testimonials = document.querySelector('.testimonials');
+const popularChallenges = document.querySelector('.popular-challenges');
 const story = document.querySelector('.story');
 const playGrid = document.querySelector('.play-grid');
 const footer = document.querySelector('.footer');
 
-
-
-
+// Buttons
 hamburger.addEventListener('click',toggleMenu);
 closeBtn.addEventListener('click', toggleMenu);
 
 
-
+// Function to toggle mobile menu
 function toggleMenu (){
     navMenu.classList.toggle('active');
     header.classList.toggle('background-state');
     hero.classList.toggle('background-state');
-    testimonial.classList.toggle('background-state');
-
+    testimonials.classList.toggle('background-state');
     popularChallenges.classList.toggle('background-state');
     story.classList.toggle('background-state');
     playGrid.classList.toggle('background-state');
